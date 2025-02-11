ec995ad0f43639a448eade1894fcaf9b106070fb
NavodContrath <navod.contrath@gmail.com>
2025-02-11 13:27:54 +0100

    add loop with FizzBuzz condition

--- main.js
+++ main.js
@@ -7,3 +7,11 @@ Scrivi un programma che stampi i numeri da 1 a 100, ma per i multipli di 3 stamp
 - declare a loop from 1 to 100
 - if-else if- else to show fizz or buzz message
 */
+
+for (let i = 1; i <= 100; ++i) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+
+    }
+
+}
