4290c299744f282ec202fae88e25a0e6715d7db7
NavodContrath <navod.contrath@gmail.com>
2025-02-11 13:19:46 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+/* Consegna:
+Scrivi un programma che stampi i numeri da 1 a 100, ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz. Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+ */
+
+/* SCHEDULE
+
+- declare a loop from 1 to 100
+- if-else if- else to show fizz or buzz message
+*/
